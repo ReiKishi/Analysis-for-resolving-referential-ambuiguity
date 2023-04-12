9e3e9392b0106384b1b1e2b8518cde570ed4f3b2
Reiki Kishimoto <97838537+ReiKishi@users.noreply.github.com>
2023-04-12 21:54:52 +0900

    Update project.R

--- project.R
+++ project.R
@@ -1,365 +1,169 @@
+#GitHubに載せるプログラム
+
 #前回の処理が影響しないように、ここでけしておく
 rm(list = ls(all.names = TRUE))
 
 #分析に必要なパッケージは最初に読んでおく
-library(magrittr)
-library(dplyr)#データフレーム処理には読み込んでおくと便利
-library(reshape2)
 library(ggplot2)
-library(tidyr)
-library(rlang)
-library(recipes)
-library(assertr)
-library(assertthat)
-library(effsize)
 library(tidyverse)
-library(magrittr)
 library(data.table)
-library(lme4)
 library(car)
-library(multcomp)
 library(ggeffects)
-library(ggplot2)
-library(ggsci)
-library(scales)
 
 
-setwd("E:/pragmatics/jikkendai")
+setwd(//)
 getwd()
 
-dat <- fread("E:/pragmatics/jikkendai/jikkendai_seijin.csv", header=TRUE)
-dat <- fread("E:/pragmatics/jikkendai/jikkendai_pooled.csv", header=TRUE)
-dat <- fread("E:/pragmatics/jikkendai/sugunikesu.csv", header=TRUE)
+dat <- fread("datafile", header=TRUE)
 
 
-##まずは誤答率の計算
-
-#不正解試行のあぶり出し
-
-#Exp1の不正解率
-dat[dat$first_target == 0 &
-      ((dat$inserted_position == "last" & dat$retention_task !=2) |
-         (dat$inserted_position == "middle" & dat$retention_task !=1)),]
-
-dat[dat$first_target == 0, ] %>% nrow()
-
-
-
-#Exp2の不正解率
-dat[dat$inserted_position == "middle" &
-      ((dat$inserted_position == "last" & dat$retention_task !=2) |
-         (dat$inserted_position == "middle" & dat$retention_task !=1)),]
-
-nrow %>% dat[dat$inserted_position == "middle", ]
-
-
-#Exp3の不正解率
-
-dat[dat$inserted_position == "last" &
-      ((dat$inserted_position == "last" & dat$retention_task !=2) |
-         (dat$inserted_position == "middle" & dat$retention_task !=1)),]
-
-nrow %>% dat[dat$inserted_position == "last", ]
-
-
-
-
-dat[(dat$inserted_position == "last" & dat$retention_task !=2) |
-      (dat$inserted_position == "middle" & dat$retention_task !=1),]
-
-
+#omit incorrect trials in a recognition task
 dat <- subset(dat,
-              (dat$inserted_position == "last" & dat$retention_task == 2) |
-                (dat$inserted_position == "middle" & dat$retention_task == 1))
-dat
-dat %>% nrow()
+              (dat$inserted_position == "#9" & dat$retention_task == 2) |
+                (dat$inserted_position == "#6" & dat$retention_task == 1))
 
 
 
-#==================================odd刺激・直近・その他だけを考える・この命令は完成====================================
+#Analysis of Experiment 1
 
+dat1 <- dat[dat$first_target==0,] #select trials of Exp. 1
 
-dat1 <- dat[dat$first_target==0,]
-
-dat1 <- mutate(dat1, category = 3)
+dat1 <- mutate(dat1, category = 3) #other strategies
 
 dat1$category[(dat1$second_target ==4 &
-                 
                  rowSums(dat1[, paste0("c", 1:9)]) ==1 &
-                 dat1$"c4" ==1
-) | 
-  (dat1$second_target ==5 &
-     rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-     dat1$"c5" ==1) |
-  (dat1$second_target ==6 &
-     rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-     dat1$"c6" == 1) |
-  (dat1$second_target ==7 &
-     rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-     dat1$"c7" == 1) |
-  (dat1$second_target ==8 &
-     rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-     dat1$"c8" == 1)] <- 1
+                 dat1$"c4" ==1) |
+                (dat1$second_target ==5 &
+                   rowSums(dat1[, paste0("c", 1:9)]) == 1 &
+                   dat1$"c5" ==1) |
+                (dat1$second_target ==6 &
+                   rowSums(dat1[, paste0("c", 1:9)]) == 1 &
+                   dat1$"c6" == 1) |
+                (dat1$second_target ==7 &
+                   rowSums(dat1[, paste0("c", 1:9)]) == 1 &
+                   dat1$"c7" == 1) |
+                (dat1$second_target ==8 &
+                   rowSums(dat1[, paste0("c", 1:9)]) == 1 &
+                   dat1$"c8" == 1)] <- 1 #selecting a rare event
 
 dat1$category[rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                dat1$"c9" == 1] <-2
+                dat1$"c9" == 1] <-2 # selecting a recent event
 
-
-dat1$category[dat1$second_target == 9 & dat1$category ==2] <-1
-
+dat1$category[dat1$second_target == 9 & dat1$category ==2] <-1 #replace "selecting a recent event" from "selecting a rare event" in #9 condition
 
 dat1 <- mutate(dat1, cat = if_else(category == 1, 1, 0))
 
-n <-3
-dat1[dat1$second_target ==4 & dat1$category == n,] %>% nrow()
-dat1[dat1$second_target ==5 & dat1$category == n,] %>% nrow()
-dat1[dat1$second_target ==6 & dat1$category == n,] %>% nrow()
-dat1[dat1$second_target ==7 & dat1$category == n,] %>% nrow()
-dat1[dat1$second_target ==8 & dat1$category == n,] %>% nrow()
-dat1[dat1$second_target ==9 & dat1$category == n,] %>% nrow()
+dat1 <- mutate(dat1, pos_contrast = ifelse(second_target == 7, -1, ifelse(second_target == 8, 0, 1)))
+dat1 <- mutate(dat1, pos_contrast_step = ifelse(second_target == 7, -0.5, ifelse(second_target == 8,-0.5,1)))
 
-dat1$second_target <- as.factor(dat1$second_target)
-l2 <- glm(cat ~ exponential + age + exponential : age, data = dat1, family = binomial)
-summary(l2)
+l1 <- glm(cat ~ pos_contrast, data = subset(dat1,dat1$age =="adults" & (dat1$second_target ==7 | dat1$second_target ==8 | dat1$second_target ==9)), family = binomial)
+l2 <- glm(cat ~ pos_contrast_step, data = subset(dat1,dat1$age =="adults" & (dat1$second_target ==7 | dat1$second_target ==8 | dat1$second_target ==9)), family = binomial)
 
-Anova(l2)
-
-summary(glht(l2, linfct = mcp(second_target = "Tukey")))
-
-plot(ggpredict(l2, terms = c("exponential", "age")), rawdata = T)
-
-a <- as.numeric(dat1$second_target)
-
-dat1 <- mutate(dat1, exponential = exp(as.numeric(dat1$second_target)))
-
-
-
-#=========================oddが２つ条件・lastだけを考える================
-
-
-dat1 <- dat[dat$first_target!=0 & dat$inserted_position =="last",]
-
-dat1 <- mutate(dat1, category = 4)
-
-
-
-dat1$category[dat1$inserted_position == "last" &
-                ((dat1$first_target != 0 & dat1$second_target == 7 &
-                    rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                    dat1$"c7" == 1) | 
-                   (dat1$first_target != 0 & dat1$second_target ==8 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                      dat1$"c8" == 1) |
-                   (dat1$first_target != 0 & dat1$second_target ==9 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 1  &
-                      dat1$"c9" == 1))]<-1
-
-dat1$category[dat1$inserted_position == "last" & 
-                ((dat1$first_target == 4 & dat1$second_target == 7 &
-                    rowSums(dat1[, paste0("c", 1:9)]) == 2 &
-                    dat1$"c4" == 1
-                  &dat1$"c7" == 1) | 
-                   (dat1$first_target == 4 & dat1$second_target ==8 &
-                      rowSums(dat1[, paste0("c", 1:9)]) ==  2 &
-                      dat1$"c4" == 1
-                    &dat1$"c8" == 1) |
-                   (dat1$first_target == 4 & dat1$second_target ==9 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 2 &
-                      dat1$"c4" == 1
-                    &dat1$"c9" == 1) |
-                   (dat1$first_target == 5 & dat1$second_target ==7 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 2 &
-                      dat1$"c5" == 1
-                    &dat1$"c7" == 1) |
-                   (dat1$first_target == 5 & dat1$second_target ==8 &
-                      rowSums(dat1[, paste0("c", 1:9)]) ==  2 &
-                      dat1$"c4" == 1
-                    &dat1$"c8" == 1) |
-                   (dat1$first_target == 5 & dat1$second_target == 9 &
-                      rowSums(dat1[, paste0("c", 1:9)]) ==  2 &
-                      dat1$"c5" == 1
-                    &dat1$"c9" == 1) |
-                   (dat1$first_target == 6 & dat1$second_target == 7 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 2 &
-                      dat1$"c6" == 1
-                    &dat1$"c7" == 1) |
-                   (dat1$first_target == 6 & dat1$second_target == 8 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 2 &
-                      dat1$"c6" == 1
-                    &dat1$"c8" == 1) |
-                   (dat1$first_target == 6 & dat1$second_target == 9 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 2 &
-                      dat1$"c6" == 1
-                    &dat1$"c9" == 1))] <- 2
-
-
-dat1$category[dat1$inserted_position == "last" &
-                dat1$second_target != 9 &
-                rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                dat1$"c9" == 1] <-3
-
-
-
-dat1$category[dat1$inserted_position == "last" &
-                ((dat1$first_target == 4 &
-                    rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                    dat1$"c4" == 1) | 
-                   (dat1$first_target == 5 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                      dat1$"c5" == 1) |
-                   (dat1$first_target == 6 &
-                      rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                      dat1$"c6" == 1))]<-5
-
-
-
-
-
-dat1 <- mutate(dat1, cat = if_else(category == 1, 1, 0))
-
-d1 <- dat1[dat1$first_target == 4,]
-
-#==================
-
-
-d1$second_target <- as.factor(d1$second_target)
-l1 <- glm(cat ~ second_target, data = d1, family = binomial)
-summary(l1)
-
-Anova(l1)
-
-summary(glht(l1, linfct = mcp(second_target = "Tukey")))
-plot(ggpredict(l1), rawdata = T)
-
-
-l0 <- glm(cat ~ 1, data = d1, family = binomial)
-
-
-#ベイズファクターの求め方。-1/2をかけて、対数をはずす。比を計算する。
-
-BIC(l1)
-BIC(l0)
-
-BIC(l0)*-1/2
-exp(BIC(l1)*(-1/2))
-exp(BIC(l0)*(-1/2))
-
-exp((BIC(l0)-BIC(l1))/2)#これがベイズファクター
-
-n <- 5
-d1[d1$second_target ==7 & d1$category ==n,] %>% nrow()
-d1[d1$second_target ==8 & d1$category ==n,] %>% nrow()
-d1[d1$second_target ==9 & d1$category ==n,] %>% nrow()
-
-#===============================
-
-
-
-#================oddが2つでmiddleに挿入されているとき====================
-
-
-dat1 <- dat[dat$first_target!=0 & dat$inserted_position =="middle",]
-
-dat1 <- mutate(dat1, category = 3)
-
-
-dat1$category[((dat1$first_target == 4 &
-                  rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                  dat1$"c4" == 1) | 
-                 (dat1$first_target == 5 &
-                    rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                    dat1$"c5" == 1) |
-                 (dat1$first_target == 6 &
-                    rowSums(dat1[, paste0("c", 1:9)]) == 1 &
-                    dat1$"c6" == 1))] <- 1
-
-dat1$category[rowSums(dat1[, paste0("c", 1:9)]) == &
-                dat1$"c6" == 1] <- 2
-
-dat1$category[dat1$first_target==6 & dat1$category ==2]<-1
-d1 <- dat1[dat1$second_target == 9,]
-d1 <- mutate(d1, cat = if_else(category == 1, 1, 0))
-
-dat1_adults <- dat1[dat1$age == "adults",]
-dat1_children <- dat1[dat1$age == "children",]
-
-
-
-
-d1$first_target <- as.factor(d1$first_target)
-
-d1 <- mutate(d1, pos_contrast = ifelse(first_target == 4, -1, ifelse(first_target == 5, 0, 1)))
-d1 <- mutate(d1, pos_contrast_step = ifelse(first_target == 4, -0.5, ifelse(first_target == 5,-0.5,1)))##4と#5は変わらないと過程しているから同じ値を入れる
-
-l1 <- glm(cat ~ pos_contrast, data = subset(d1,d1$age =="children"), family = binomial)
-l2 <- glm(cat ~ pos_contrast_step, data = subset(d1,d1$age =="children"), family = binomial)
-
-summary(l1)#linear
-summary(l2)#段階
-
-Anova(l1)
-Anova(l2)
-
-summary(glht(l1, linfct = mcp(first_target = "Tukey")))
+summary(l1)#linear model
+summary(l2)#step model
 
 plot(ggpredict(l1), rawdata = T)#linearはpositionによって上がっていっている
 plot(ggpredict(l2), rawdata = T)#段階は#4と#5は同じで#6でだけ上がっている
 
 
-#AICが低いほどあてはまりがいい。
+#Analysis of Experiment 2
 
 
-n <-3
-d1[d1$first_target ==4 & d1$category == n,] %>% nrow()
-d1[d1$first_target ==5 & d1$category == n,] %>% nrow()
-d1[d1$first_target ==6 & d1$category == n,] %>% nrow()
+dat2 <- dat[dat$inserted_position =="#6",] #select trials of Exp. 2
+
+dat2 <- mutate(dat2, category = 3)#other strategies
+
+dat2$category[(rowSums(dat2[, paste0("c", 1:9)]) == 1 &
+                 ((dat2$first_target == 4 &
+                     dat2$"c4" == 1) |
+                    (dat2$first_target == 5 &
+                       dat2$"c5" == 1) |
+                    (dat2$first_target == 6 &
+                       dat2$"c6" == 1)))] <- 1 #selecting a rare event
+
+dat2$category[rowSums(dat2[, paste0("c", 1:9)]) == 1 &
+                dat2$"c6" == 1] <- 2 #selecting a recent event
+
+dat2$category[dat2$first_target == 6 & dat2$category == 2] <- 1 #replace "selecting a recent event" from "selecting a rare event" in #9 condition
+
+dat2 <- dat2[dat2$second_target == 7,]
+
+dat2 <- mutate(dat2, cat = if_else(category == 1, 1, 0))
+
+dat2 <- mutate(dat2, pos_contrast = ifelse(first_target == 4, -1, ifelse(first_target == 5, 0, 1)))
+dat2 <- mutate(dat2, pos_contrast_step = ifelse(first_target == 4, -0.5, ifelse(first_target == 5,-0.5,1)))
+
+l1 <- glm(cat ~ pos_contrast, data = subset(dat2,dat2$age =="children"), family = binomial)
+l2 <- glm(cat ~ pos_contrast_step, data = subset(dat2,dat2$age =="children"), family = binomial)
+
+summary(l1)#linear model
+summary(l2)#step model
+
+plot(ggpredict(l1), rawdata = T)#linearはpositionによって上がっていっている
+plot(ggpredict(l2), rawdata = T)#段階は#4と#5は同じで#6でだけ上がっている
 
 
 
+#Analysis of Experiment 3
 
-write.csv(d1,"E:/pragmatics/data.csv")
+dat3 <- dat[dat$inserted_position =="#9",]
 
-
-
-
-
-
-#==============oddが2つ、Distracted、Non-Distractedで6th以降の選択があったかなかったかを検討する
-
-f <- 6
-s <- 9
-
-test_data <- dat[dat$first_target == f &
-                   dat$second_target == s,]
-
-test_data <- mutate(test_data, cat = 1)
-
-#一つでも#7以降に反応があった＝1
-#cat 0は7以降に反応がなかった
-test_data$cat[(test_data$"c7"==0 &
-                 test_data$"c8" ==0 &
-                 test_data$"c9"== 0)] <- 0
-
-if (all(test_data$cat[test_data$inserted_position == "last"]==1)){
-  test_data$cat[test_data$inserted_position == "last" & test_data$cat == 1][1] <-0
-}
+dat3 <- mutate(dat3, category = 4)#other strategies
 
 
-test_data$inserted_position <- as.factor(test_data$inserted_position)
-l2 <- glm(cat ~ inserted_position, data = test_data, family = binomial)
-summary(l2)
-Anova(l2)
+inserted_cond <- dat3$inserted_position == "#9"
+row_sum_cond <- rowSums(dat3[, paste0("c", 1:9)]) == 1
+second_targets <- dat3$second_target %in% 7:9
+first_target_not_zero <- dat3$first_target != 0
 
-summary(glht(l2, linfct = mcp(inserted_position = "Tukey")))
+dat3$category[inserted_cond & row_sum_cond] <- ifelse(
+  inserted_cond & row_sum_cond & ((second_targets & dat3[, paste0("c", dat3$second_target)] == 1) |
+                                    (first_target_not_zero & dat3$second_target == 9 & dat3$c9 == 1)),
+  1,
+  dat3x$category)#selecting a rare event
+
+
+targets <- expand.grid(first = 4:6, second = 7:9)
+row_sum_cond <- rowSums(dat1[, paste0("c", 1:9)]) == 2
+inserted_cond <- dat3$inserted_position == "#9"
+
+dat3$category[inserted_cond & row_sum_cond] <- sapply(1:nrow(dat3), function(i) {
+  if (!inserted_cond[i] || !row_sum_cond[i]) return(dat3$category[i])
+  
+  for (j in 1:nrow(targets)) {
+    first_target_col <- paste0("c", targets$first[j])
+    second_target_col <- paste0("c", targets$second[j])
+    
+    if (dat3$first_target[i] == targets$first[j] &&
+        dat3$second_target[i] == targets$second[j] &&
+        dat3[[first_target_col]][i] == 1 &&
+        dat3[[second_target_col]][i] == 1) {
+      return(2)
+    }
+  }
+  
+  return(dat3$category[i])
+})#selecting a rare event and distractor
+
+
+
+dat3$category[dat3$inserted_position == "#9" &
+                dat3$second_target != 9 &
+                rowSums(dat1[, paste0("c", 1:9)]) == 1 &
+                dat1$"c9" == 1] <-3 #selecting a recent event
+
+dat3 <- mutate(dat3, cat = if_else(category == 1, 1, 0))
+
+dat3 <- dat1[dat1$first_target == 4,]#example
+
+dat3 <- mutate(dat3, pos_contrast = ifelse(first_target == 7, -1, ifelse(second_target == 8, 0, 1)))
+dat3 <- mutate(dat3, pos_contrast_step = ifelse(first_target == 7, -0.5, ifelse(second_target == 8,-0.5,1)))
+
+l1 <- glm(cat ~ pos_contrast, data = subset(dat1,dat1$age =="children"), family = binomial)
+l2 <- glm(cat ~ pos_contrast_step, data = subset(dat1,dat1$age =="children"), family = binomial)
+
+summary(l1)#linear model
+summary(l2)#step model
+
+plot(ggpredict(l1), rawdata = T)
 plot(ggpredict(l2), rawdata = T)
-
-
-#===============================
-
-
-
-a<-rnorm(100, 1, 0.1)
-b<-rnorm(100, 2, 0.1)
-
-write.csv(a,"E:/pragmatics/jasp_data_a.csv")
-write.csv(b,"E:/pragmatics/jasp_data_b.csv")
